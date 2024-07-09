e372db6f80222700db44ab8a5baa60ceda01fdb1
Nicolas Salles <nicolassalles.ing@gmail.com>
2024-07-08 23:08:51 -0300

    Inicio del proyecto

---
+++ Practico1.py
@@ -0,0 +1,8 @@
+"""
+Práctico 1 Diplomatura Datos
+Clara Cabrera - Marianela Luján - Nicolás Salles
+"""
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+print('Módulos importados exitosamente!')
