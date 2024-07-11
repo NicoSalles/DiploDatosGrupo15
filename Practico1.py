6c4d9ea3c4b90eba8ab5ce7b2049161bcbaf760e
Nicolas Salles <nicolassalles.ing@gmail.com>
2024-07-11 18:10:42 -0300

    Inicia el script con un resumen de la base

--- Practico1.py
+++ Practico1.py
@@ -5,4 +5,11 @@ Clara Cabrera - Marianela Luján - Nicolás Salles
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-print('Módulos importados exitosamente!')
+
+
+#Generamos un objeto DataFrame de pandas
+tabla=pd.read_csv('heart_diseases.csv')
+print('Se muestran las primeras 5 y las últimas 5 filas\n', tabla)
+
+#Generamos una pequeña estadística
+print('Resumen \n',tabla.describe())
